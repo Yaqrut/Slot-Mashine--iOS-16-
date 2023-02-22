3424ed778c19e2a57509b48307d100df3a64957d
Viktor Mazur <market.brocars@gmail.com>
2023-02-22 13:05:45 -0800

    Initial Commit

---
+++ Slot Mashine/Slot_MashineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MashineApp.swift
+//  Slot Mashine
+//
+//  Created by YAQRUT on 2023-02-22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MashineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
