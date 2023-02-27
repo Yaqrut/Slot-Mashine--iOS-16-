96b90c70832c7f07642f52b328e001e9efc69458
Viktor Mazur <market.brocars@gmail.com>
2023-02-26 20:54:04 -0800

    Create Extension.swift

---
+++ Slot Mashine/Helpers/Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Extension.swift
+//  Slot Mashine
+//
+//  Created by YAQRUT on 2023-02-22.
+//
+
+import SwiftUI
+
+extension Text {
+    func scoreLabelStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(size: 10, weight: .bold, design: .rounded))
+    }
+    
+    func scoreNumberStyle() -> Text {
+        self
+            .foregroundColor(Color.white)
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.heavy)
+    }
+}
