ef4a24134b47e6205e880187f08f3a6caf75f5d8
Viktor Mazur <market.brocars@gmail.com>
2023-02-26 20:54:13 -0800

    Create PlaySound.swift

---
+++ Slot Mashine/Helpers/PlaySound.swift
@@ -0,0 +1,22 @@
+//
+//  PlaySound.swift
+//  Slot Mashine
+//
+//  Created by YAQRUT on 2023-02-26.
+//
+
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("ERROR: Could not find and play the sound file!")
+        }
+    }
+}
+
